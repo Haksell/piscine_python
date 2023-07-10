cfb85c9c826d2c556689fc4e4dd2181d8c5868fd
Axel Brisse <axel.brisse@gmail.com>
2023-07-10 20:53:52 +0200

    day0/ex03

---
+++ day0/ex02/find_ft_type.py
@@ -0,0 +1,19 @@
+import builtins
+
+
+def all_thing_is_obj(obj: any) -> int:
+    t = type(obj)
+    match t:
+        case builtins.list:
+            print("List :", t)
+        case builtins.tuple:
+            print("Tuple :", t)
+        case builtins.set:
+            print("Set :", t)
+        case builtins.dict:
+            print("Dict :", t)
+        case builtins.str:
+            print("Brian is in the kitchen :", t)
+        case _:
+            print("Type not found")
+    return 42
