e18597969ed4e359cbf86589c1b96eefc0582537
Axel Brisse <axel.brisse@gmail.com>
2023-07-10 23:18:13 +0200

    day0/ex07

---
+++ day0/ex07/sos.py
@@ -0,0 +1,56 @@
+import sys
+
+NESTED_MORSE = {
+    " ": "/",
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+}
+
+
+def main(argv):
+    try:
+        assert len(argv) == 2
+        s = argv[1].upper()
+        assert type(s) == str and all(c in NESTED_MORSE for c in s)
+    except AssertionError:
+        print("AssertionError: the arguments are bad")
+    else:
+        print(" ".join(NESTED_MORSE[c] for c in s))
+
+
+if __name__ == "__main__":
+    main(sys.argv)
