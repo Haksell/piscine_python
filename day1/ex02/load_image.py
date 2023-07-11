6ac57be310b20c8b18cd99cfe3e0f6f3478cd49c
Axel Brisse <axel.brisse@gmail.com>
2023-07-11 02:15:56 +0200

    mypy day1/ex02

--- day1/ex02/load_image.py
+++ day1/ex02/load_image.py
@@ -2,7 +2,7 @@ from PIL import Image
 import numpy as np
 
 
-def ft_load(path: str) -> np.array:
+def ft_load(path: str) -> np.ndarray | None:
     """Load an image from a file into a numpy array."""
     try:
         img = Image.open(path)
