e7119314d2228460d0633a5cb879548eda016640
Axel Brisse <axel.brisse@gmail.com>
2023-09-28 22:05:07 +0200

    .2e for 0/01

--- day0/ex01/format_ft_time.py
+++ day0/ex01/format_ft_time.py
@@ -3,7 +3,7 @@ from datetime import datetime
 now = datetime.now()
 epoch = now.timestamp()
 print(
-    f"Seconds since January 1, 1970: {epoch:,.4f} or {epoch:.3g}",
+    f"Seconds since January 1, 1970: {epoch:,.4f} or {epoch:.2e}",
     "in scientific notation",
 )
 print(now.strftime("%b %d %Y"))
