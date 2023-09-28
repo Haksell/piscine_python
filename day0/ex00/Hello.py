d36036f0ade56eef20dcaa648bfeefa11f7f9da7
Axel Brisse <axel.brisse@gmail.com>
2023-09-28 22:01:00 +0200

    retarded fix for 0/00

--- day0/ex00/Hello.py
+++ day0/ex00/Hello.py
@@ -1,7 +1,15 @@
 ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello": "titi!"}
+
+ft_list[1] = "World!"
 ft_tuple = ("Hello", "France!")
-ft_set = {"Hello", "Paris!"}
-ft_dict = {"Hello": "42Paris!"}
+ft_set.discard("tutu!")
+ft_set.add("Paris")
+# sets in Python are not ordered, so we need to sort it before printing
+ft_set = "{" + str(sorted(ft_set))[1:-1] + "}"
+ft_dict["Hello"] = "42Paris!"
 
 print(ft_list)
 print(ft_tuple)
