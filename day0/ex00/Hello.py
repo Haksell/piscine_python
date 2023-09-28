27aa298a8097e772432ff1bf326cec6bdf47f5ca
Axel Brisse <axel.brisse@gmail.com>
2023-09-28 22:18:37 +0200

    000: overkill solution

--- day0/ex00/Hello.py
+++ day0/ex00/Hello.py
@@ -3,12 +3,17 @@ ft_tuple = ("Hello", "toto!")
 ft_set = {"Hello", "tutu!"}
 ft_dict = {"Hello": "titi!"}
 
+
+class OrderedSet(set):
+    def __str__(self):
+        return "{" + str(sorted(self))[1:-1] + "}"
+
+
 ft_list[1] = "World!"
 ft_tuple = ("Hello", "France!")
 ft_set.discard("tutu!")
 ft_set.add("Paris")
-# sets in Python are not ordered, so we need to sort it before printing
-ft_set = "{" + str(sorted(ft_set))[1:-1] + "}"
+ft_set = OrderedSet(ft_set)
 ft_dict["Hello"] = "42Paris!"
 
 print(ft_list)
