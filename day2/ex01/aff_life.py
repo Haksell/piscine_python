bd70969200a8573808f6720a12729eb079a8d1bd
Axel Brisse <axel.brisse@gmail.com>
2023-09-30 02:21:36 +0200

    201

---
+++ day2/ex01/aff_life.py
@@ -0,0 +1,19 @@
+from load_csv import load
+import matplotlib.pyplot as plt
+
+
+def aff_life():
+    """Draws a graph of life expectancy in France from 1800 to 2100."""
+    df = load("../data/life_expectancy_years.csv")
+    df = df[df["country"] == "France"]
+    df = df.set_index("country")
+    df = df.transpose()
+    df.plot(figsize=(6, 5), legend=False)
+    plt.title("France life expectancy projections")
+    plt.xlabel("Year")
+    plt.ylabel("Life expectancy")
+    plt.show()
+
+
+if __name__ == "__main__":
+    aff_life()
