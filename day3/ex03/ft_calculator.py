42a563afa5ac74b993cfd74e2e2aa67187a4b19d
Axel Brisse <axel.brisse@gmail.com>
2023-09-30 05:43:23 +0200

    303 actually

--- day3/ex03/ft_calculator.py
+++ day3/ex03/ft_calculator.py
@@ -8,24 +8,24 @@ class calculator:
     def __add__(self, x) -> None:
         """Add a number to all the numbers in the calculator."""
         self.data = [d + x for d in self.data]
-        return self.data
+        print(self.data)
 
     def __mul__(self, x) -> None:
         """Multiply all the numbers in the calculator by a number."""
         self.data = [d * x for d in self.data]
-        return self.data
+        print(self.data)
 
     def __sub__(self, x) -> None:
         """Subtract a number from all the numbers in the calculator."""
         self.data = [d - x for d in self.data]
-        return self.data
+        print(self.data)
 
     def __truediv__(self, x) -> None:
         """Divide all the numbers in the calculator by a number."""
         if x == 0:
             raise ZeroDivisionError(f"trying to divide {self!r} by 0")
         self.data = [d / x for d in self.data]
-        return self.data
+        print(self.data)
 
     def __repr__(self) -> str:
         """Return a debugging representation of the object."""
