7187d50aa5c57e7575e5ccc606c0c5695a3ce748
Axel Brisse <axel.brisse@gmail.com>
2023-09-30 02:54:00 +0200

    202 good? plz check

--- day2/ex02/aff_pop.py
+++ day2/ex02/aff_pop.py
@@ -1,5 +1,6 @@
 from load_csv import load
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
 
 
 def parse_pop(x):
@@ -23,7 +24,11 @@ def aff_pop():
     df = df.applymap(parse_pop)
     df = df.loc[:, "1800":"2050"]
     df = df.transpose()
-    df.plot(figsize=(6, 5))
+    print(df)
+    _, ax = plt.subplots(figsize=(6, 5))
+    df.plot(ax=ax)
+    ax.yaxis.set_major_formatter(mticker.FuncFormatter(lambda x, pos: f"{x/1e6:.0f}M"))
+    ax.set_yticks([i * 2e7 for i in range(9)])
     plt.title("Population Projections")
     plt.xlabel("Year")
     plt.ylabel("Population")
